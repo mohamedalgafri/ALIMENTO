2987c65a16335bd00b738be5b2905b68bdae9be8
mohamedalgafri <mohamed18540m@gamil.com>
2022-10-24 12:22:51 +0300

    up2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () =>{
+  const header = document.getElementById('navbar')
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  this.scrollY >= 0 ? header.classList.add('scroll-nav') 
+                     : header.classList.remove('scroll-nav')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
 /////////////////////////////////////swiper header/////////////////////////////////////////////
 
 var swiper = new Swiper(".mySwiperheder", {
