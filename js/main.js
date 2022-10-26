e1c8cd0b8d6f21af94f4a2cfc02d2ddc22244f4e
mohamedalgafri <mohamed18540m@gamil.com>
2022-10-26 15:06:06 +0300

    up10

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ const scrollHeader = () =>{
 window.addEventListener('scroll', scrollHeader)
 
 
+
+
 /////////////////////////////////////swiper header/////////////////////////////////////////////
 
 var swiper = new Swiper(".mySwiperheder", {
@@ -70,3 +72,15 @@ var swiper = new Swiper(".mySwiperPro", {
     },
   },
 });
+
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
+
+/////////////////////////////////////////////////////////
